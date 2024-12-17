daa9b1b68535d1822cd3d38f851a200d979b5078
Prachi Pandey <pandeyprachi8572@gmail.com>
2024-12-17 17:45:41 +0530

    first commit

--- api/index.js
+++ api/index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import mongoose from 'mongoose';
+mongoose.connect()
 const app = express();
 app.listen(3000 ,()=>{
     console.log('Server is running on prot 3000');
