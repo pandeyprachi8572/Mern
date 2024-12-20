d24315020985a42f2fc981d2b2442f4fc631443c
Prachi Pandey <pandeyprachi8572@gmail.com>
2024-12-20 11:58:55 +0530

    *

--- api/index.js
+++ api/index.js
@@ -1,7 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
-mongoose.connect()
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(()=>{
+console.log('Connrcted to MongoDB!');
+}).catch((err)=>{
+    console.log(err);
+});
 const app = express();
-app.listen(3000 ,()=>{
-    console.log('Server is running on prot 3000');
-})
+app.listen(3000 , ()=>{
+    console.log('Server is running on prot 3000!');
+}
+)
