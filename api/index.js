fdf9a4bfb20112a317e64818e7e00ea772b1d3c6
Prachi Pandey <pandeyprachi8572@gmail.com>
2024-12-20 17:30:17 +0530

    *

--- api/index.js
+++ api/index.js
@@ -23,3 +23,14 @@ app.get('/test', (req , res)=>{
 );
 app.use( "/api/user", userRouter );
 app.use('/api/auth' , authRouter);
+// this is middle ware 
+app.use((err ,req ,res ,next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success : false ,
+        statusCode,
+        message,
+    })
+
+})
